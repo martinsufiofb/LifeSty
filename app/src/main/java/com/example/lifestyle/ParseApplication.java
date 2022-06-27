2a8338c9b57d3507552f73f10f39a7d894abf0a1
martinsufiofb <106703322+martinsufiofb@users.noreply.github.com>
2022-06-27 15:40:26 -0700

    Registered Pushups, Situps and Squats classes
    Registered Pushups, situps and squats classes from parse database so I can be able to make changes from code

--- app/src/main/java/com/example/lifestyle/ParseApplication.java
+++ app/src/main/java/com/example/lifestyle/ParseApplication.java
@@ -14,6 +14,9 @@ public class ParseApplication extends Application {
         ParseObject.registerSubclass(Exercise.class);
         ParseObject.registerSubclass(User.class);
         ParseObject.registerSubclass(History.class);
+        ParseObject.registerSubclass(Pushups.class);
+        ParseObject.registerSubclass(Situps.class);
+        ParseObject.registerSubclass(Squats.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("ZV1oqmS1nwMiS9S8gw325hWMljYaH1vMtITOi4QD")
