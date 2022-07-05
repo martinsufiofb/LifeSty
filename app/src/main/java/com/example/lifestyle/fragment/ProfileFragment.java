8c2736e70ee114a263bcb9200244d7637801430e
martinsufiofb <106703322+martinsufiofb@users.noreply.github.com>
2022-07-05 12:00:04 -0700

    Renamed hm1 to hmSorted to give more details

--- app/src/main/java/com/example/lifestyle/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/lifestyle/fragment/ProfileFragment.java
@@ -13,14 +13,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.example.lifestyle.Exercise;
-import com.example.lifestyle.ExercisesAdapter;
 import com.example.lifestyle.History;
 import com.example.lifestyle.HistoryAdapter;
 import com.example.lifestyle.LoginActivity;
@@ -228,8 +225,8 @@ public class ProfileFragment extends Fragment {
         hm.put("Sit ups", situpsTotal);
         hm.put("Squats", squatsTotal);
         int i = 0;
-        Map<String, Integer> hm1 = sortByValue(hm);
-        for (Map.Entry<String, Integer> en : hm1.entrySet()) {
+        Map<String, Integer> hmSorted = sortByValue(hm);
+        for (Map.Entry<String, Integer> en : hmSorted.entrySet()) {
             l1.get(i).setText(String.valueOf(en.getValue()));
             l2.get(i).setText(String.valueOf(en.getKey()));
             i++;
