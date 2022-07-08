0f3467d1c54e7c72b0c1d3273eb6df9190534a1d
martinsufiofb <106703322+martinsufiofb@users.noreply.github.com>
2022-07-08 11:00:42 -0700

    Added Fragments for each exercises where I would implement the automated counting logic

---
+++ app/src/main/java/com/example/lifestyle/CameraActivity.java
@@ -0,0 +1,36 @@
+package com.example.lifestyle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.lifestyle.fragment.PushupsCounterFragment;
+import com.example.lifestyle.fragment.SitupsCounterFragment;
+import com.example.lifestyle.fragment.SquatCounterFragment;
+
+import java.util.HashMap;
+
+public class CameraActivity extends AppCompatActivity {
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    HashMap<Integer, Fragment> fragments = new HashMap<>();
+    Fragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        fragments.put(1,new PushupsCounterFragment());
+        fragments.put(2,new SitupsCounterFragment());
+        fragments.put(3,new SquatCounterFragment());
+
+        Intent intent = getIntent();
+        int exerciseChosen = intent.getIntExtra("exerciseClicked",0);
+        fragment = fragments.get(exerciseChosen);
+
+        fragmentManager.beginTransaction().replace(R.id.flContainer,fragment).commit();
+
+    }
+}
---
+++ app/src/main/java/com/example/lifestyle/fragment/SquatCounterFragment.java
@@ -0,0 +1,55 @@
+package com.example.lifestyle.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lifestyle.R;
+
+public class SquatCounterFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+    public SquatCounterFragment() {
+        // Required empty public constructor
+    }
+
+    public static SquatCounterFragment newInstance(String param1, String param2) {
+        SquatCounterFragment fragment = new SquatCounterFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_squat_counter, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+}
