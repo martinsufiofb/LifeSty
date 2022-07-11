f393487519ced6b7a5272ffeb36161caaf80ea34
martinsufiofb <106703322+martinsufiofb@users.noreply.github.com>
2022-07-10 21:42:01 -0700

    Added space to keep consistency

--- app/src/main/java/com/example/lifestyle/User.java
+++ app/src/main/java/com/example/lifestyle/User.java
@@ -2,10 +2,12 @@ package com.example.lifestyle;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 @ParseClassName("User")
 public class User extends ParseObject {
     public static final  String KEY_TITLE = "username";
+
     public String getUsername(){
         return getString(KEY_TITLE);
     }
