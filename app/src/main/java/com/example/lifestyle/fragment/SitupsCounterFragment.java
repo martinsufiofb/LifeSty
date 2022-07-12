8d2bfa2c1b50a3f0d28cf620d72a04f946fd4322
martinsufiofb <106703322+martinsufiofb@users.noreply.github.com>
2022-07-12 16:21:41 -0700

    Added done button and cancel button

--- app/src/main/java/com/example/lifestyle/fragment/SitupsCounterFragment.java
+++ app/src/main/java/com/example/lifestyle/fragment/SitupsCounterFragment.java
@@ -1,44 +1,35 @@
 package com.example.lifestyle.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.lifestyle.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SitupsCounterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SitupsCounterFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    int result_code = 11;
+    CardView doneButton;
+    CardView cancelButton;
+    TextView numberOfSitups;
+    int noOfSitups = 0;
 
     public SitupsCounterFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SitupsCounterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SitupsCounterFragment newInstance(String param1, String param2) {
         SitupsCounterFragment fragment = new SitupsCounterFragment();
         Bundle args = new Bundle();
@@ -58,9 +49,38 @@ public class SitupsCounterFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_situps_counter, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        doneButton = view.findViewById(R.id.situpsCounterDoneBtn);
+        cancelButton = view.findViewById(R.id.situpsCounterCancelBtn);
+        numberOfSitups = view.findViewById(R.id.tvNoOfSitups);
+        Intent intent = getActivity().getIntent();
+        String pushups  = intent.getStringExtra("numberOfPushups");
+        numberOfSitups.setText(pushups);
+        noOfSitups = Integer.parseInt(pushups);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("result", noOfSitups);
+                getActivity().setResult(result_code, intent);
+                getActivity().finish();
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                getActivity().setResult(result_code, intent);
+                getActivity().finish();
+            }
+        });
     }
 }
