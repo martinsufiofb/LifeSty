311bbe5552b725d5b8762446ee315f3f6d773e9b
martinsufiofb <106703322+martinsufiofb@users.noreply.github.com>
2022-07-14 14:35:53 -0700

    added set userId to the history class, and also changed the format of KEY_NAME_OF_EXERCISE

--- app/src/main/java/com/example/lifestyle/History.java
+++ app/src/main/java/com/example/lifestyle/History.java
@@ -8,14 +8,15 @@ import com.parse.ParseUser;
 @ParseClassName("History")
 public class History extends ParseObject {
     public static final String KEY_COUNT = "count";
-    public static final String KEY_NAMEOFEXERCISE = "nameOfExercise";
+    public static final String KEY_NAME_OF_EXERCISE = "nameOfExercise";
     public static final String KEY_USER = "user";
+    public static final String KEY_USER_ID = "userId";
 
     public String getCount(){
         return getString(KEY_COUNT);
     }
     public String getNameOfExercise(){
-        return getString(KEY_NAMEOFEXERCISE);
+        return getString(KEY_NAME_OF_EXERCISE);
     }
     public ParseUser getUser(){
         return getParseUser(KEY_USER);
@@ -24,7 +25,8 @@ public class History extends ParseObject {
     public void  setCount(String count){
         put(KEY_COUNT, count);
     }
-    public void  setNameOfExercise(String nameofexercise){put(KEY_NAMEOFEXERCISE, nameofexercise);}
+    public void  setNameOfExercise(String nameofexercise){put(KEY_NAME_OF_EXERCISE, nameofexercise);}
     public void setUser(ParseUser user){put(KEY_USER, user);}
+    public void setUserId(String userId){put(KEY_USER_ID, userId);}
 
 }
