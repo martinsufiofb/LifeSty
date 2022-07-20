c13c9c6434063108686750749f27ab937ab60fa8
martinsufiofb <106703322+martinsufiofb@users.noreply.github.com>
2022-07-19 18:24:12 -0700

    moved history adapter class into adapter package

--- app/src/main/java/com/example/lifestyle/adapters/HistoryAdapter.java
+++ app/src/main/java/com/example/lifestyle/adapters/HistoryAdapter.java
@@ -1,4 +1,4 @@
-package com.example.lifestyle;
+package com.example.lifestyle.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -8,6 +8,9 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lifestyle.History;
+import com.example.lifestyle.R;
 
 import java.util.List;
 
