2109b8f807b2ef93c2d381ca33fe936ca77f5bae
martinsufiofb <106703322+martinsufiofb@users.noreply.github.com>
2022-07-19 18:25:55 -0700

    moved graph class to utils package

---
+++ app/src/main/java/com/example/lifestyle/utils/Graph.java
@@ -0,0 +1,37 @@
+package com.example.lifestyle.utils;
+
+import com.example.lifestyle.History;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+public class Graph {
+    public static String[] setXAxis(int noOfDays) {
+        String[] orderedDays = new String[noOfDays];
+        for(int i = 0; i< noOfDays; i++){
+            Date currentTime = Calendar.getInstance().getTime();
+            Calendar day7ago = Calendar.getInstance();
+            day7ago.setTime(currentTime);
+            day7ago.add(Calendar.DAY_OF_YEAR, -(noOfDays -i-1));
+            Date newDate = day7ago.getTime();
+            String day = newDate.toString().substring(0,10);
+            orderedDays[i] = day;
+        }
+        return orderedDays;
+    }
+
+    public static HashMap<String, String> addDuplicateDate(List<History> exercise) {
+        HashMap<String, String> hm = new HashMap<>();
+        for (int i = 0; i < exercise.size(); i++) {
+            String day = exercise.get(i).getCreatedAt().toString().substring(0, 10);
+            if (hm.containsKey(day)) {
+                hm.put(day, Integer.toString(Integer.parseInt(hm.get(day)) + Integer.parseInt(exercise.get(i).getCount())));
+            } else {
+                hm.put(day, exercise.get(i).getCount());
+            }
+        }
+        return hm;
+    }
+}
